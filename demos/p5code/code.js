7c7f0e24abcee4d504c80048289bd61083c93a94
Jack B. Du <JackBDu@nyu.edu>
2015-03-18 18:54:04 +0800

    "code" for p5 added

---
+++ demos/p5code/code.js
@@ -0,0 +1,361 @@
+/**
+ * Blockly Demos: Code
+ *
+ * Copyright 2012 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview p5dotjs for Blockly's Code demo.
+ * @author fraser@google.com (Neil Fraser)
+ */
+'use strict';
+
+/**
+ * Create a namespace for the application.
+ */
+var Code = {};
+
+/**
+ * Extracts a parameter from the URL.
+ * If the parameter is absent default_value is returned.
+ * @param {string} name The name of the parameter.
+ * @param {string} defaultValue Value to return if paramater not found.
+ * @return {string} The parameter value or the default value if not found.
+ */
+Code.getStringParamFromUrl = function(name, defaultValue) {
+  var val = location.search.match(new RegExp('[?&]' + name + '=([^&]+)'));
+  return val ? decodeURIComponent(val[1].replace(/\+/g, '%20')) : defaultValue;
+};
+
+/**
+ * Load blocks saved on App Engine Storage or in session/local storage.
+ * @param {string} defaultXml Text representation of default blocks.
+ */
+Code.loadBlocks = function(defaultXml) {
+  try {
+    var loadOnce = window.sessionStorage.loadOnceBlocks;
+  } catch(e) {
+    // Firefox sometimes throws a SecurityError when accessing sessionStorage.
+    // Restarting Firefox fixes this, so it looks like a bug.
+    var loadOnce = null;
+  }
+  if ('BlocklyStorage' in window && window.location.hash.length > 1) {
+    // An href with #key trigers an AJAX call to retrieve saved blocks.
+    BlocklyStorage.retrieveXml(window.location.hash.substring(1));
+  } else if (loadOnce) {
+    // Language switching stores the blocks during the reload.
+    delete window.sessionStorage.loadOnceBlocks;
+    var xml = Blockly.Xml.textToDom(loadOnce);
+    Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+  } else if (defaultXml) {
+    // Load the editor with default starting blocks.
+    var xml = Blockly.Xml.textToDom(defaultXml);
+    Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+  } else if ('BlocklyStorage' in window) {
+    // Restore saved blocks in a separate thread so that subsequent
+    // initialization is not affected from a failed load.
+    window.setTimeout(BlocklyStorage.restoreBlocks, 0);
+  }
+};
+
+/**
+ * Bind a function to a button's click event.
+ * On touch enabled browsers, ontouchend is treated as equivalent to onclick.
+ * @param {!Element|string} el Button element or ID thereof.
+ * @param {!Function} func Event handler to bind.
+ */
+Code.bindClick = function(el, func) {
+  if (typeof el == 'string') {
+    el = document.getElementById(el);
+  }
+  el.addEventListener('click', func, true);
+  el.addEventListener('touchend', func, true);
+};
+
+/**
+ * Load the Prettify CSS and p5dotjs.
+ */
+Code.importPrettify = function() {
+  //<link rel="stylesheet" href="../prettify.css">
+  //<script src="../prettify.js"></script>
+  var link = document.createElement('link');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('href', '../prettify.css');
+  document.head.appendChild(link);
+  var script = document.createElement('script');
+  script.setAttribute('src', '../prettify.js');
+  document.head.appendChild(script);
+};
+
+/**
+ * Compute the absolute coordinates and dimensions of an HTML element.
+ * @param {!Element} element Element to match.
+ * @return {!Object} Contains height, width, x, and y properties.
+ * @private
+ */
+Code.getBBox_ = function(element) {
+  var height = element.offsetHeight;
+  var width = element.offsetWidth;
+  var x = 0;
+  var y = 0;
+  do {
+    x += element.offsetLeft;
+    y += element.offsetTop;
+    element = element.offsetParent;
+  } while (element);
+  return {
+    height: height,
+    width: width,
+    x: x,
+    y: y
+  };
+};
+
+/**
+ * List of tab names.
+ * @private
+ */
+Code.TABS_ = ['blocks', 'p5dotjs', 'xml'];
+
+Code.selected = 'blocks';
+
+/**
+ * Switch the visible pane when a tab is clicked.
+ * @param {string} clickedName Name of tab clicked.
+ */
+Code.tabClick = function(clickedName) {
+  // If the XML tab was open, save and render the content.
+  if (document.getElementById('tab_xml').className == 'tabon') {
+    var xmlTextarea = document.getElementById('content_xml');
+    var xmlText = xmlTextarea.value;
+    var xmlDom = null;
+    try {
+      xmlDom = Blockly.Xml.textToDom(xmlText);
+    } catch (e) {
+      var q =
+          window.confirm(MSG['badXml'].replace('%1', e));
+      if (!q) {
+        // Leave the user on the XML tab.
+        return;
+      }
+    }
+    if (xmlDom) {
+      Blockly.mainWorkspace.clear();
+      Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xmlDom);
+    }
+  }
+
+  // Deselect all tabs and hide all panes.
+  for (var i = 0; i < Code.TABS_.length; i++) {
+    var name = Code.TABS_[i];
+    document.getElementById('tab_' + name).className = 'taboff';
+    document.getElementById('content_' + name).style.visibility = 'hidden';
+  }
+
+  // Select the active tab.
+  Code.selected = clickedName;
+  document.getElementById('tab_' + clickedName).className = 'tabon';
+  // Show the selected pane.
+  document.getElementById('content_' + clickedName).style.visibility =
+      'visible';
+  Code.renderContent();
+  Blockly.fireUiEvent(window, 'resize');
+};
+
+/**
+ * Populate the currently selected pane with content generated from the blocks.
+ */
+Code.renderContent = function() {
+  var content = document.getElementById('content_' + Code.selected);
+  // Initialize the pane.
+  if (content.id == 'content_xml') {
+    var xmlTextarea = document.getElementById('content_xml');
+    var xmlDom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+    var xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+    xmlTextarea.value = xmlText;
+    xmlTextarea.focus();
+  } else if (content.id == 'content_p5dotjs') {
+    var code = Blockly.p5dotjs.workspaceToCode();
+    content.textContent = code;
+    if (typeof prettyPrintOne == 'function') {
+      code = content.innerHTML;
+      code = prettyPrintOne(code, 'js');
+      content.innerHTML = code;
+    }
+  } else if (content.id == 'content_python') {
+    code = Blockly.Python.workspaceToCode();
+    content.textContent = code;
+    if (typeof prettyPrintOne == 'function') {
+      code = content.innerHTML;
+      code = prettyPrintOne(code, 'py');
+      content.innerHTML = code;
+    }
+  } else if (content.id == 'content_dart') {
+    code = Blockly.Dart.workspaceToCode();
+    content.textContent = code;
+    if (typeof prettyPrintOne == 'function') {
+      code = content.innerHTML;
+      code = prettyPrintOne(code, 'dart');
+      content.innerHTML = code;
+    }
+  }
+};
+
+/**
+ * Initialize Blockly.  Called on page load.
+ */
+Code.init = function() {
+  Code.initLanguage();
+
+  // Disable the link button if page isn't backed by App Engine storage.
+  var linkButton = document.getElementById('linkButton');
+  if ('BlocklyStorage' in window) {
+    BlocklyStorage['HTTPREQUEST_ERROR'] = MSG['httpRequestError'];
+    BlocklyStorage['LINK_ALERT'] = MSG['linkAlert'];
+    BlocklyStorage['HASH_ERROR'] = MSG['hashError'];
+    BlocklyStorage['XML_ERROR'] = MSG['xmlError'];
+    Code.bindClick(linkButton, BlocklyStorage.link);
+  } else if (linkButton) {
+    linkButton.className = 'disabled';
+  }
+
+  var container = document.getElementById('content_area');
+  var onresize = function(e) {
+    var bBox = Code.getBBox_(container);
+    for (var i = 0; i < Code.TABS_.length; i++) {
+      var el = document.getElementById('content_' + Code.TABS_[i]);
+      el.style.top = bBox.y + 'px';
+      el.style.left = bBox.x + 'px';
+      // Height and width need to be set, read back, then set again to
+      // compensate for scrollbars.
+      el.style.height = bBox.height + 'px';
+      el.style.height = (2 * bBox.height - el.offsetHeight) + 'px';
+      el.style.width = bBox.width + 'px';
+      el.style.width = (2 * bBox.width - el.offsetWidth) + 'px';
+    }
+    // Make the 'Blocks' tab line up with the toolbox.
+    if (Blockly.mainWorkspace.toolbox_.width) {
+      document.getElementById('tab_blocks').style.minWidth =
+          (Blockly.mainWorkspace.toolbox_.width - 38) + 'px';
+          // Account for the 19 pixel margin and on each side.
+    }
+  };
+  window.addEventListener('resize', onresize, false);
+
+  var toolbox = document.getElementById('toolbox');
+  Blockly.inject(document.getElementById('content_blocks'),
+      {grid:
+          {spacing: 25,
+           length: 3,
+           colour: '#ccc',
+           snap: true},
+       media: '../../media/',
+       toolbox: toolbox});
+
+  // Add to reserved word list: Local variables in execution evironment (runJS)
+  // and the infinite loop detection function.
+  Blockly.p5dotjs.addReservedWords('code,timeouts,checkTimeout');
+
+  Code.loadBlocks('');
+
+  if ('BlocklyStorage' in window) {
+    // Hook a save function onto unload.
+    BlocklyStorage.backupOnUnload();
+  }
+
+  Code.tabClick(Code.selected);
+  Blockly.fireUiEvent(window, 'resize');
+
+  Code.bindClick('trashButton',
+      function() {Code.discard(); Code.renderContent();});
+  Code.bindClick('runButton', Code.runJS);
+
+  for (var i = 0; i < Code.TABS_.length; i++) {
+    var name = Code.TABS_[i];
+    Code.bindClick('tab_' + name,
+        function(name_) {return function() {Code.tabClick(name_);};}(name));
+  }
+
+  // Lazy-load the syntax-highlighting.
+  window.setTimeout(Code.importPrettify, 1);
+};
+
+/**
+ * Initialize the page language.
+ */
+Code.initLanguage = function() {
+  // Set the HTML's language and direction.
+  // document.dir fails in Mozilla, use document.body.parentNode.dir instead.
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=151407
+
+  // Inject language strings.
+  document.title += ' ' + MSG['title'];
+  document.getElementById('title').textContent = MSG['title'];
+  document.getElementById('tab_blocks').textContent = MSG['blocks'];
+
+  document.getElementById('linkButton').title = MSG['linkTooltip'];
+  document.getElementById('runButton').title = MSG['runTooltip'];
+  document.getElementById('trashButton').title = MSG['trashTooltip'];
+
+  var categories = ['catLogic', 'catEnvironment', 'catShapes', 'catStructures', 'catRenderings', 'catLoops', 'catMath', 'catText',
+                    'catColour', 'catVariables'];
+  for (var i = 0, cat; cat = categories[i]; i++) {
+    document.getElementById(cat).setAttribute('name', MSG[cat]);
+  }
+  var textVars = document.getElementsByClassName('textVar');
+  for (var i = 0, textVar; textVar = textVars[i]; i++) {
+    textVar.textContent = MSG['textVariable'];
+  }
+  var listVars = document.getElementsByClassName('listVar');
+  for (var i = 0, listVar; listVar = listVars[i]; i++) {
+    listVar.textContent = MSG['listVariable'];
+  }
+};
+
+/**
+ * Execute the user's code.
+ * Just a quick and dirty eval.  Catch infinite loops.
+ */
+Code.runJS = function() {
+  Blockly.p5dotjs.INFINITE_LOOP_TRAP = '  checkTimeout();\n';
+  var timeouts = 0;
+  var checkTimeout = function() {
+    if (timeouts++ > 1000000) {
+      throw MSG['timeout'];
+    }
+  };
+  var code = Blockly.p5dotjs.workspaceToCode();
+  Blockly.p5dotjs.INFINITE_LOOP_TRAP = null;
+  try {
+    eval(code);
+  } catch (e) {
+    alert(MSG['badCode'].replace('%1', e));
+  }
+};
+
+/**
+ * Discard all blocks from the workspace.
+ */
+Code.discard = function() {
+  var count = Blockly.mainWorkspace.getAllBlocks().length;
+  if (count < 2 ||
+      window.confirm(MSG['discard'].replace('%1', count))) {
+    Blockly.mainWorkspace.clear();
+    window.location.hash = '';
+  }
+};
+
+window.addEventListener('load', Code.init);
